1461b31cbec45a27066c25bd02baecc4ed1c8ca0
Matt Reeds <59578678+MReeds@users.noreply.github.com>
2020-09-28 12:45:28 -0500

    Update ApacheCassandra.py

--- ApacheCassandra.py
+++ ApacheCassandra.py
@@ -18,14 +18,43 @@ try:
 except Exception as e:
     print(e)
     
+###Set Keyspace
 try:
     session.set_keyspace('Music')
 except Exception as e:
     print(e)
     
+    
+###Create Table
 query = "CREATE TABLE IF NOT EXISTS Music"
 query = query + "(artist_name text, song_title text, year int, album_name text, single boolean, PRIMARY KEY (year, artist_name))"
 try:
     session.execute(query)
 except Exception as e:
     print(e)
+
+    
+###Add values to table
+query = "Music" 
+query = query + " VALUES (%s, %s, %s, %s, %s)"
+
+try:
+    session.execute(query, ('The Beatles', 'Across the Universe', 1970, 'Let It Be', False))
+except Exception as e:
+    print(e)
+    
+try:
+    session.execute(query, ('The Beatles', 'Think for Yourself', 1965, 'Rubber Soul', True))
+except Exception as e:
+    print(e)
+
+    
+###Select values from table
+query = 'SELECT * FROM Music'
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
+    
+for row in rows:
+    print (row.year, row.album_name, row.artist_name)
